cc4e785ca1dc4f5fba1a9aa6e724bdecbe3987b8
ashutoshshukla0 <reactashutoshshukla@gmail.com>
2026-04-14 18:36:58 +0530

    chore: extract TYPE_COLORS into shared constants file

---
+++ pokemon-kata/src/constants/typeColors.js
@@ -0,0 +1,11 @@
+export const TYPE_COLORS = {
+  fire: '#FF6B35', water: '#4ECDC4', grass: '#7BC67E', electric: '#FFE66D',
+  psychic: '#C77DFF', ice: '#74C7EC', dragon: '#7B68EE', dark: '#4A4E69',
+  fairy: '#FFB3DE', normal: '#A8A77A', fighting: '#C22E28', flying: '#A98FF3',
+  poison: '#A33EA1', ground: '#E2BF65', rock: '#B6A136', bug: '#A6B91A',
+  ghost: '#735797', steel: '#B7B7CE',
+}
+
+
+
+
